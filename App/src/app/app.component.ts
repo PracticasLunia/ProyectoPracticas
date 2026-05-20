65fb511f220df1132b00a5ee0464f38e74823311
jgsanchez <jgsanchez@lunia.es>
2026-05-20 08:48:31 +0200

    configuración StatusBar

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { Capacitor } from '@capacitor/core';
+import { StatusBar, Style } from '@capacitor/status-bar';
 import { MenuComponent } from "./shared/components/menu/menu";
 
 @Component({
@@ -8,5 +10,14 @@ import { MenuComponent } from "./shared/components/menu/menu";
   imports: [IonApp, IonRouterOutlet, MenuComponent],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    this.initStatusBar();
+  }
+
+  private async initStatusBar() {
+    if (!Capacitor.isNativePlatform()) return;
+    await StatusBar.setOverlaysWebView({ overlay: false });
+    await StatusBar.setStyle({ style: Style.Dark });
+    await StatusBar.setBackgroundColor({ color: '#3880ff' });
+  }
 }
